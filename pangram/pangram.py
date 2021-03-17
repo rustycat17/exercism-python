afe304ecd94b07a3ea3818ccc6c09554a7bd0a40
rustycat17 <rustycat17@posteo.nz>
2021-03-17 22:32:36 +0100

    added function for pangram project

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,10 @@
+import string
+
 def is_pangram(sentence):
-    pass
+    res = []
+    for letter in sentence.lower():
+        if letter in string.ascii_lowercase:
+            res.append(letter)
+
+    res = set(res)
+    return True if len(res) == 26 else False
