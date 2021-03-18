afc2ba9e1bccfaf3e95cf23dd970f3d43b2bfaee
rustycat17 <rustycat17@posteo.nz>
2021-03-18 13:08:26 +0100

    added solution for protein-translation

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,2 +1,31 @@
+d = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan",
+    "UAA": "",
+    "UAG": "",
+    "UGA": "",
+     }
+
+
 def proteins(strand):
-    pass
+    i = 0
+    res = []
+    while i < len(strand):
+        if len(d[strand[i:i+3]]) == 0:
+            return res
+        res.append(d[strand[i:i+3]])
+        i += 3
+
+    return res
