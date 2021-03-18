4814d0f2b5bfd34259b3c656d76dac6214f0f746
rustycat17 <rustycat17@posteo.nz>
2021-03-18 11:00:02 +0100

    implemented solution for robot-name

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,16 @@
+import random
+import string
+
+
 class Robot:
     def __init__(self):
-        pass
+        random.seed(random.randint(1, 1e6))
+        letters = "".join(random.choice(string.ascii_uppercase) for x in range(2))
+        numbers = "".join(random.choice(string.digits) for x in range(3))
+        name = letters + numbers
+
+        self.name = name
+
+    def reset(self):
+        random.randint(1, 1e9)
+        self.__init__()
