c235c3c2407c2694e8bc2e0874c9b3811c0a2d45
rustycat17 <rustycat17@posteo.nz>
2021-03-19 06:59:31 +0100

    added solution for perfect-numbers project

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,2 +1,15 @@
 def classify(number):
-    pass
+    if number < 1:
+        raise ValueError("number must be larger than 0")
+
+    res = 0
+    for i in range(1, number // 2 + 1):
+        if number % i == 0:
+            res += i
+
+    if res == number:
+        return "perfect"
+    elif res > number:
+        return "abundant"
+    else:
+        return "deficient"
